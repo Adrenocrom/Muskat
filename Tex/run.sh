beab5d580130bc0d79a305fcb672ae02a507caf5
Josef Schulz <rbd00r@gmx.de>
2016-10-17 11:26:24 +0200

    u

---
+++ Tex/run.sh
@@ -0,0 +1,16 @@
+pdflatex diplominf.tex
+pdflatex diplominf.tex
+bibtex diplominf.aux
+pdflatex diplominf.tex
+pdflatex diplominf.tex
+
+rm diplominf.bbl
+rm diplominf.aux
+rm diplominf.blg
+rm diplominf.log
+rm diplominf.out
+rm diplominf.pgf-plot.gnuplot
+rm diplominf.synctex.gz
+rm diplominf.toc
+rm texput.log
+rm test.png
