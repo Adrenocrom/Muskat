3e9540a16e2ab892fa544197213515a3b5b82eea
Josef Schulz <rbd00r@gmx.de>
2016-12-09 15:42:44 +0100

    u

--- Tex/run.sh
+++ Tex/run.sh
@@ -14,12 +14,12 @@ bibtex diplominf.aux
 pdflatex diplominf.tex
 pdflatex diplominf.tex
 
-rm diplominf.bbl
-rm diplominf.aux
-rm diplominf.blg
-rm diplominf.log
-rm diplominf.out
-rm diplominf.pgf-plot.gnuplot
-rm diplominf.synctex.gz
-rm diplominf.toc
-rm texput.log
+#rm diplominf.bbl
+#rm diplominf.aux
+#rm diplominf.blg
+#rm diplominf.log
+#rm diplominf.out
+#rm diplominf.pgf-plot.gnuplot
+#rm diplominf.synctex.gz
+#rm diplominf.toc
+#rm texput.log
