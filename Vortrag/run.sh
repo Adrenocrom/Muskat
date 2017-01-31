fadc43510eecdbdd2283fc8c57a239d8505f156f
Josef Schulz <rbd00r@gmx.de>
2017-01-31 10:44:35 +0100

    u

---
+++ Vortrag/run.sh
@@ -0,0 +1,24 @@
+rm Verteidigung.aux
+rm Verteidigung.bbl
+rm Verteidigung.blg
+rm Verteidigung.log
+rm Verteidigung.nav
+rm Verteidigung.out
+rm Verteidigung.pdf
+rm Verteidigung.snm
+rm Verteidigung.toc
+
+pdflatex Verteidigung.tex
+pdflatex Verteidigung.tex
+bibtex	 Verteidigung.aux
+pdflatex Verteidigung.tex
+pdflatex Verteidigung.tex
+
+rm Verteidigung.aux
+rm Verteidigung.bbl
+rm Verteidigung.blg
+rm Verteidigung.log
+rm Verteidigung.nav
+rm Verteidigung.out
+rm Verteidigung.snm
+rm Verteidigung.toc
